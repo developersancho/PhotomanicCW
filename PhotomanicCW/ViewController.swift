12d0eed3f011da8d22be6b6ed2395bb548b9635d
developersancho <developersancho@gmail.com>
2018-10-29 01:35:03 +0300

    Initial Commit

---
+++ PhotomanicCW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotomanicCW
+//
+//  Created by developersancho on 29.10.2018.
+//  Copyright © 2018 developersancho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
